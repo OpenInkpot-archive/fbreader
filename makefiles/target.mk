2839fbb15ca922967dc02b7f62923a10f1a1c8c0
Alexander Egorov <lunohod@gmx.de>
2008-02-14 00:39:52 +0100

    Adding new nanox ui infrastructure

--- makefiles/target.mk
+++ makefiles/target.mk
@@ -1,3 +1,6 @@
+#TARGET_ARCH = lbook
+#UI_TYPE = nanox
+
 #TARGET_ARCH = openzaurus
 #UI_TYPE = opie
 #UI_TYPE = gpe
@@ -13,7 +16,8 @@
 #UI_TYPE = maemo4
 
 TARGET_ARCH = desktop
-UI_TYPE = gtk
+UI_TYPE = nanox
+#UI_TYPE = gtk
 #UI_TYPE = qt
 #UI_TYPE = qt4
 
