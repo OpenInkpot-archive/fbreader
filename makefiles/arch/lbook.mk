2fdf6ca891bf2d51802c01b43d0d252c29b80848
Alexander Egorov <lunohod@gmx.de>
2008-02-17 12:37:17 +0100

    lbook target makefile

---
+++ makefiles/arch/lbook.mk
@@ -0,0 +1,27 @@
+include $(ROOTDIR)/makefiles/arch/unix.mk
+
+ifeq "$(INSTALLDIR)" ""
+  INSTALLDIR=/usr
+endif
+IMAGEDIR = $(INSTALLDIR)/share/pixmaps
+APPIMAGEDIR = $(INSTALLDIR)/share/pixmaps/%APPLICATION_NAME%
+
+CC = arm-linux-gcc
+AR = arm-linux-ar rsu
+LD = arm-linux-g++
+
+CFLAGS = -pipe -fno-exceptions -Wall -Wno-ctor-dtor-privacy -W -DLIBICONV_PLUG -DARM
+LDFLAGS =
+
+ifeq "$(UI_TYPE)" "nanox"
+  UILIBS = -L$(ROOTDIR)/v3/arm/lib -lnano-X -lrt -lpthread
+  NXINCLUDE = -I$(ROOTDIR)/v3/include/nanox/
+  EXTERNALINCLUDE = -I$(ROOTDIR)/v3/include/
+  ZLSHARED = no
+endif
+
+XML_LIB = -L$(ROOTDIR)/v3/arm/lib -lexpat
+ARCHIVER_LIB = -L$(ROOTDIR)/v3/arm/lib -lzlib -lbz2
+
+RM = rm -rvf
+RM_QUIET = rm -rf
